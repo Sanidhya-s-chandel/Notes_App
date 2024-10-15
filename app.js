37b1e6fc51b5b6ba128d29c9609a6c5e7522b8d2
sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2024-10-15 15:29:24 +0530

    Almost completed

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const app = express();
+const fs = require("fs")
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('index');
+    fs.readdir("./public",{withFileTypes:true},(err,files)=>{
+        res.render('index',{files})
+    })
 });
 
 app.get('/new', (req, res) => {
     res.render('new');
 });
+
+app.get("/submit",(req,res)=>{
+    fs.writeFile(`./public/${req.query.fileName}.txt`,req.query.Data,(err)=>{
+        if(err) throw err
+        else res.render("complete")
+    })
+})
 
 app.listen(3000);
