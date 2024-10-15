88007a04dcda0ab0be694b584bcac56d8c80b84b
sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2024-10-15 16:39:51 +0530

    Done

--- app.js
+++ app.js
@@ -2,23 +2,40 @@ const express = require('express');
 const app = express();
 const fs = require("fs")
 
+app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    fs.readdir("./public",{withFileTypes:true},(err,files)=>{
-        res.render('index',{files})
-    })
+    fs.readdir("./public", { withFileTypes: true }, (err, files) => {
+        if (err) throw err;
+        const fileDescriptions = files.map(file => {
+            if (file.isFile()) {
+                const fileName = file.name;
+                const description = fs.readFileSync(`./public/${fileName}`, 'utf8').split('\n')[0];
+                return { name: fileName, description: description };
+            }
+            return { name: file.name, description: '' };
+        });
+        res.render('index', { files: fileDescriptions });
+    });
 });
 
 app.get('/new', (req, res) => {
     res.render('new');
 });
 
-app.get("/submit",(req,res)=>{
-    fs.writeFile(`./public/${req.query.fileName}.txt`,req.query.Data,(err)=>{
+app.post("/submit",(req,res)=>{
+    fs.writeFile(`./public/${req.body.fileName}.txt`,`${req.body.noteDescription}\n\n${req.body.Data}`,(err)=>{
         if(err) throw err
         else res.render("complete")
+    })
+})
+
+app.get("/delete",(req,res)=>{
+    fs.unlink(`./public/${req.query.fileName}`,(err)=>{
+        if(err) throw err
+        else res.render("del")
     })
 })
 
